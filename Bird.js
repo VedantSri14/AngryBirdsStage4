917cf2e3845ef4f2408df2dd14fb0aa0e26f5728
VedantSri14 <srivastavavishal.vs@gmail.com>
2020-10-19 14:03:59 +0530

    Adding a slingshot

--- Bird.js
+++ Bird.js
@@ -1,12 +1,12 @@
 class Bird extends BaseClass {
-  constructor(x,y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
-  }
+	constructor(x, y) {
+		super(x, y, 50, 50);
+		this.image = loadImage("sprites/bird.png");
+	}
 
-  display() {
-    this.body.position.x = mouseX;
-    this.body.position.y = mouseY;
-    super.display();
-  }
+	display() {
+		//this.body.position.x = mouseX;
+		//this.body.position.y = mouseY;
+		super.display();
+	}
 }
